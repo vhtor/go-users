8364205463e2f3fbc86eafc40bedab747034d614
Victor Hugo <victor.hugo@esig.com.br>
2023-10-31 22:13:01 -0300

    Created JSON binding on create_user route

--- src/controller/create_user.go
+++ src/controller/create_user.go
@@ -1,11 +1,23 @@
 package controller
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	resterr "github.com/vhtor/metaifrn-simulados-api/src/configuration/rest_err"
+	"github.com/vhtor/metaifrn-simulados-api/src/controller/model/request"
 )
 
 func CreateUser(ctx *gin.Context) {
-	err := resterr.NewBadRequestError("This route was called improperly")
-	ctx.JSON(err.Code, err)
+	var userRequest request.UserRequest
+
+	if err := ctx.ShouldBindJSON(&userRequest); err != nil {
+		restErr := resterr.NewBadRequestError(
+			fmt.Sprintf("There are incorrect fields. Error = %s", err.Error()),
+		)
+		ctx.JSON(restErr.Code, restErr)
+		return
+	}
+	
+	fmt.Println(userRequest)
 }
