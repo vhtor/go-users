754873140aaafdcb8d06c192d42f6a72242314c4
Victor Hugo <victor.hugo@esig.com.br>
2023-12-11 20:22:19 -0300

    Added mongodb connection constructor

--- src/configuration/database/mongodb/mongodb_connection.go
+++ src/configuration/database/mongodb/mongodb_connection.go
@@ -2,23 +2,36 @@ package mongodb
 
 import (
 	"context"
+	"os"
 
 	logger "github.com/vhtor/metaifrn-simulados-api/src/configuration/log"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func InitConnection() {
-	ctx := context.Background()
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
-	
+var (
+	MONGODB_URL_KEY = "MONGODB_URL"
+	MONGODB_USER_DB_KEY = "MONGODB_USER_DB"
+)
+
+func NewMongoDBConnection(
+	ctx context.Context,
+) (*mongo.Database, error) {
+	url := os.Getenv(MONGODB_URL_KEY)
+
+	client, err := mongo.Connect(
+		ctx,
+		options.Client().ApplyURI(url),
+	)
+
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	if err = client.Ping(ctx, nil); err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	logger.Info("Connected to MongoDB")
+	return client.Database(MONGODB_USER_DB_KEY), nil
 }
